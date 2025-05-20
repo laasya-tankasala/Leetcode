e0c7f8bb63b01e5ec6911994a02d775a35212a60
Laasya Tankasala <63518023+laasya-tankasala@users.noreply.github.com>
2025-05-21 03:03:10 +0530

    Update MinimumSizeSubarraySum.java

--- SlidingWindow/MinimumSizeSubarraySum.java
+++ SlidingWindow/MinimumSizeSubarraySum.java
@@ -6,7 +6,7 @@ Given an array of positive integers nums and a positive integer target,
 return the minimal length of a subarray whose sum is greater than or equal to target.
 If there is no such subarray, return 0 instead.
 
-@see https://leetcode.com/problems/minimum-size-subarray-sum/description/
+@link https://leetcode.com/problems/minimum-size-subarray-sum/description/
  */
 class MinimumSizeSubarraySum{
 
