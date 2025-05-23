bc354ac9d69b2cadcc48da9b343b4fa68a634ce4
Laasya Tankasala <63518023+laasya-tankasala@users.noreply.github.com>
2025-05-23 11:37:15 +0530

    Math Easy

--- src/main/java/interview150/math/SqrtOfX.java
+++ src/main/java/interview150/math/SqrtOfX.java
@@ -12,16 +12,17 @@ package interview150.math;
 */
 public class SqrtOfX {
 
-    /*
+    /*  Using loop
         we need the max. possible value of i such that i*i <= x
         so, increment i until i<=x/i;
         Q: Why i*i <= x is not the while loop condition?
-        Ans: Because for larger integers, value of i*i would be greater than max integer value.
+        Ans: Because for larger integers, value of i*i would be greater than max integer value and the condition may not
+        work as intended.
 
         time O(n)
         space O(1)
      */
-    public int mySqrt(int x) {
+    public int sqrt(int x) {
 
         if(x<2) return x;
 
@@ -31,5 +32,32 @@ public class SqrtOfX {
 
         return i-1;
 
+    }
+
+    /*
+        Using binary search
+        time O(logn)
+        space O(1)
+     */
+    public int mySqrt(int x) {
+
+        if(x==0) return 0;
+
+        int res = 1;
+        int low = 1, high = x;
+
+        while(low<=high) {
+
+            int mid = low + (high-low)/2;
+
+            if(mid <= x/mid) {
+                res = mid;
+                low = mid+1;
+            } else
+                high = mid -1;
+
+        }
+
+        return res;
     }
 }
